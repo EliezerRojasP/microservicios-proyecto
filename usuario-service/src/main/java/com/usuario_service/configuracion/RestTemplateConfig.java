cb2a96f964d9b6c3434e0095b0ff705a4156cfbb
EliezerRojasP <EliezerRojasPantigoso@hotmail.com>
2024-10-22 02:41:32 -0500

    Prometheus

--- usuario-service/src/main/java/com/usuario_service/configuracion/RestTemplateConfig.java
+++ usuario-service/src/main/java/com/usuario_service/configuracion/RestTemplateConfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-	
-	@Bean
-	@LoadBalanced
-    public RestTemplate restTemplate() {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
         return new RestTemplate();
     }
 	

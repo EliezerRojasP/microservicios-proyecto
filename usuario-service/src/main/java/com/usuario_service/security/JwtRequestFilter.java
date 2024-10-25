f63e785410860ecd97bcbe6fcfea20cf42d12462
EliezerRojasP <EliezerRojasPantigoso@hotmail.com>
2024-10-24 19:18:58 -0500

    Security

--- usuario-service/src/main/java/com/usuario_service/security/JwtRequestFilter.java
+++ usuario-service/src/main/java/com/usuario_service/security/JwtRequestFilter.java
@@ -19,7 +19,7 @@ import java.util.Date;
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    private String SECRET_KEY = "";  // Almacenar en lugar seguro
+    private String SECRET_KEY = "${JWT_SECRET}";  // Almacenar en lugar seguro
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
---
+++ usuario-service/src/main/java/com/usuario_service/security/OAuth2Controller.java
@@ -0,0 +1,5 @@
+package com.usuario_service.security;
+
+public class OAuth2Controller {
+
+}

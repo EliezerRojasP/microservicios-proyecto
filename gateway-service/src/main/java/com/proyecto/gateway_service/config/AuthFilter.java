8cf5b0498502149e92a40b7e89557096e56064b0
EliezerRojasP <EliezerRojasPantigoso@hotmail.com>
2024-10-25 21:05:14 -0500

    auth-jwt

--- gateway-service/src/main/java/com/proyecto/gateway_service/config/AuthFilter.java
+++ gateway-service/src/main/java/com/proyecto/gateway_service/config/AuthFilter.java
@@ -3,7 +3,6 @@ package com.proyecto.gateway_service.config;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,42 +16,37 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
-    private final WebClient.Builder webClientBuilder;
+    private WebClient.Builder webClient;
 
-    public AuthFilter(WebClient.Builder webClientBuilder) {
+    public AuthFilter(WebClient.Builder webClient) {
         super(Config.class);
-        this.webClientBuilder = webClientBuilder;
+        this.webClient = webClient;
     }
-
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                return onError(exchange, HttpStatus.UNAUTHORIZED);
-            }
-
-            String tokenHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-            if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
+        return (((exchange, chain) -> {
+            if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION))
                 return onError(exchange, HttpStatus.BAD_REQUEST);
-            }
-
-            String token = tokenHeader.substring(7);
-
-            return webClientBuilder.build()
+            String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String [] chunks = tokenHeader.split(" ");
+            if(chunks.length != 2 || !chunks[0].equals("Bearer"))
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            return webClient.build()
                     .post()
-                    .uri("http://auth-service/auth/validate?token=" + token)
-                    .retrieve()
-                    .bodyToMono(TokenDto.class)
-                    .flatMap(tokenDto -> chain.filter(exchange))
-                    .onErrorResume(e -> onError(exchange, HttpStatus.UNAUTHORIZED));
-        };
+                    .uri("http://auth-service/auth/validate?token=" + chunks[1])
+                    .retrieve().bodyToMono(TokenDto.class)
+                    .map(t -> {
+                        t.getToken();
+                        return exchange;
+                    }).flatMap(chain::filter);
+        }));
     }
 
-    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
-        exchange.getResponse().setStatusCode(status);
-        return exchange.getResponse().setComplete();
+    public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status){
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
     }
 
-    public static class Config {
-    }
+    public static class Config {}
 }

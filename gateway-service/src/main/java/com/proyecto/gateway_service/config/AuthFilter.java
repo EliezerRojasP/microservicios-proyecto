f97f0a837f5121fcb59c89ce0525b97f88302cd0
EliezerRojasP <EliezerRojasPantigoso@hotmail.com>
2024-10-25 23:41:18 -0500

    auth-jwt

--- gateway-service/src/main/java/com/proyecto/gateway_service/config/AuthFilter.java
+++ gateway-service/src/main/java/com/proyecto/gateway_service/config/AuthFilter.java
@@ -1,10 +1,9 @@
 package com.proyecto.gateway_service.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
@@ -15,50 +14,45 @@ import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
 @Component
-public class AuthFilter implements GlobalFilter, Ordered {
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
-    private final WebClient.Builder webClient;
+    private WebClient.Builder webClient;
 
-    @Autowired
     public AuthFilter(WebClient.Builder webClient) {
+        super(Config.class);
         this.webClient = webClient;
     }
-
+    
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // Verificar si el encabezado de autorización existe
-        if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-            return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
-        }
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
 
-        String tokenHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
-            return onError(exchange, "Token format invalid", HttpStatus.BAD_REQUEST);
-        }
+            String tokenHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            }
 
-        String token = tokenHeader.substring(7);
+            String token = tokenHeader.substring(7);
 
-        // Verificar el token mediante auth-service
-        return webClient.build()
-                .post()
-                .uri("http://auth-service/auth/validate")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .retrieve()
-                .bodyToMono(TokenDto.class)
-                .flatMap(t -> chain.filter(exchange))  // Si es válido, continuar con la solicitud
-                .onErrorResume(e -> onError(exchange, "Token validation failed", HttpStatus.UNAUTHORIZED));
+            return webClient.build()
+                    .post()
+                    .uri("http://auth-service/auth/validate")
+                    .bodyValue(token)
+                    .retrieve()
+                    .bodyToMono(TokenDto.class)
+                    .flatMap(t -> chain.filter(exchange));
+        };
     }
 
-    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
-        exchange.getResponse().setStatusCode(httpStatus);
-        return exchange.getResponse().setComplete();
+
+    public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status){
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
     }
 
-    @Override
-    public int getOrder() {
-        return -1;
-    }
-
-	
+    public static class Config {}
 }
-
